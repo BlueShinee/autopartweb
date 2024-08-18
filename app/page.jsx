b3fc89b79c087a3ed616a18b7fd805445e687e6f
7A7z <sadaminatou@gmail.com>
2024-08-18 07:37:39 +0530

    Update page.jsx

--- app/page.jsx
+++ app/page.jsx
@@ -4,5 +4,5 @@ import Navbar from "@/components/navbar"
 export default function Home() {
   return (
     <Navbar brandname="Bus Part Motors" /*max character width of 15ch*/ /* Contactmenustate={Contactmenustate} */ /* menufunc={Contactmenustatechange} *//>
-  );
+  )
 }
