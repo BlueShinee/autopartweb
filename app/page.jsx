23fd7ef9f528d48754d5c87f515d3031223e5c63
7A7z <sadaminatou@gmail.com>
2024-08-19 13:03:50 +0530

    Contact menu
    Ported the contact menu from v2 react to v3 next , and fixed some things as well as had to make the root page.jsx "use client"

--- app/page.jsx
+++ app/page.jsx
@@ -1,8 +1,18 @@
+"use client"
+
 import Image from "next/image";
 import Navbar from "@/components/navbar"
+import Contactmenu from "@/components/contactmenu";
+import { useState } from "react";
 
 export default function Home() {
+
+  let [Contactmenustate , Contactmenustatechange] = useState(false)
+
   return (
-    <Navbar brandname="Bus Part Motors" /*max character width of 15ch*/ /* Contactmenustate={Contactmenustate} */ /* menufunc={Contactmenustatechange} *//>
+    <>
+      <Contactmenu Contactmenustate={Contactmenustate} menufunc={Contactmenustatechange}/>
+      <Navbar brandname="Bus Part Motors" /*max character width of 15ch*/ /* Contactmenustate={Contactmenustate} */ menufunc={Contactmenustatechange}/>
+    </>
   )
 }
