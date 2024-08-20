4fea40f2a4cb7c1cc4d8cdfb3a3f739e96fa566e
7A7z <sadaminatou@gmail.com>
2024-08-20 23:39:35 +0530

    item listing

--- app/Items/[itemid]/page.jsx
+++ app/Items/[itemid]/page.jsx
@@ -6,10 +6,18 @@ import Image from "next/image";
 export default async function page({params}) {
     const pb = new PocketBase('http://127.0.0.1:8090');
     const record = await pb.collection('items').getOne(params.itemid)
+    const photos = record.photos
+    console.log(record);
+    
     return (
         <div className="flex flex-col">
             <Link href={"/"} className="flex justify-center items-center py-1 m-2 hover:bg-blue-500 transition-all active:scale-95   px-4 bg-blue-400 w-24 rounded-md"><span className="text-white font-medium text-lg mr-2">Back</span><Image src={"/back-arrow.svg"}  width={20} height={20}/></Link>
-            {record.name}
+            <div className="w-full flex flex-col justify-center items-center">
+                <img src={pb.files.getUrl(record, photos[0], {'thumb': '100x250'})} className="w-[90%] rounded-lg shadow-xl"/>
+                <div>
+                    
+                </div>
+            </div>
         </div>
     )
 }
