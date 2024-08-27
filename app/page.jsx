96e1253e777c390df381d6cfb694e60868c89218
7A7z <sadaminatou@gmail.com>
2024-08-27 17:53:26 +0530

    update

--- app/page.jsx
+++ app/page.jsx
@@ -31,7 +31,7 @@ export default async function Home() {
 
   return (
     <>
-      <Header/>
+      <Header />
       <Slideshow src1="https://images.unsplash.com/photo-1497436072909-60f360e1d4b1?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2560&q=80"
       
       src2="https://i.pinimg.com/originals/bd/ec/ea/bdecea0788a51112bd9355850f1b1697.jpg"
