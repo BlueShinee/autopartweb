d51ad4a482742e00abdb20e0b1ce8c6b661e15a9
EveloCore <kumuthuprabhasha@gmail.com>
2024-08-27 20:41:59 +0530

    full element overflow bug fixed

--- app/layout.jsx
+++ app/layout.jsx
@@ -10,12 +10,11 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
+    <html lang="en" style={{overflowX: "hidden"}}>
       <head>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="stylesheet" href="/animations.css" />
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css"/>
-             <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css"/>         
       </head>
       <body className={inter.className}>{children}</body>
     </html>
