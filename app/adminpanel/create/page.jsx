7b09c893e9981a2b695e74ac0e0a2e8d96992aee
7A7z <sadaminatou@gmail.com>
2024-08-28 01:22:35 +0530

    create

--- app/adminpanel/create/page.jsx
+++ app/adminpanel/create/page.jsx
@@ -7,13 +7,13 @@ export default async function page() {
 
         <form action="" className="flex flex-col p-4">
             <span className="text-sm text-gray-600 font-medium mt-4">Item Name</span>
-            <input type="text" name="itemname" defaultValue={""} className="border-2 border-gray-400 rounded-md p-1 focus:border-blue-500 outline-none"/>
+            <input type="text" name="itemname" defaultValue={""} className="border-2 border-gray-400 rounded-md p-1 focus:border-blue-500 outline-none" required/>
 
 
             <span className="text-sm text-gray-600 font-medium mt-2">Price</span>
-            <input type="number" name="price" defaultValue={""} className="border-2 border-gray-400 rounded-md w-24 p-1 focus:border-blue-500 outline-none"/>
+            <input type="number" name="price" defaultValue={""} className="border-2 border-gray-400 rounded-md w-24 p-1 focus:border-blue-500 outline-none" required/>
             <div className="flex justify-center items-center">
-                <button type="submit" className="mt-12 w-32 py-2 px-4">Create +</button>
+                <button type="submit" className="mt-12 w-32 py-2 px-4 bg-green-500 transition-all rounded-md hover:bg-green-600 text-white font-semibold">Create +</button>
             </div>
         </form>
     </div>
